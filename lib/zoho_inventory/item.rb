010089fe5be14d24406e10b778891dc3508e5616
Vincent Rolea <vincent.rolea@gmail.com>
2018-11-08 16:27:08 +0100

    Add item class

---
+++ lib/zoho_inventory/item.rb
@@ -0,0 +1,22 @@
+module ZohoInventory
+  class Item
+    def resource_name
+      "items"
+    end
+
+    def self.list
+    end
+
+    def self.create(params = {})
+    end
+
+    def self.retrieve(id)
+    end
+
+    def update(id, params = {})
+    end
+
+    def delete(id)
+    end
+  end
+end
