f64025478a84b0a9e8e80f16e918231c048d4279
Vincent Rolea <vincent.rolea@lhtp.fr>
2019-01-08 17:49:29 +0100

    Add Organization CRUD operationsé

--- lib/zoho_inventory/util.rb
+++ lib/zoho_inventory/util.rb
@@ -3,14 +3,16 @@ module ZohoInventory
     def self.object_classes
       @object_classes ||= {
         Item::OBJECT_NAME => Item,
-        Contact::OBJECT_NAME => 'Contact'
+        Contact::OBJECT_NAME => Contact,
+        Organization::OBJECT_NAME => Organization
       }
     end
 
     def self.remote_object_key
       @remote_object_keys ||= {
         Item::OBJECT_NAME => 'item',
-        Contact::OBJECT_NAME => 'contact'
+        Contact::OBJECT_NAME => 'contact',
+        Organization::OBJECT_NAME => 'organization'
       }
     end
   end
